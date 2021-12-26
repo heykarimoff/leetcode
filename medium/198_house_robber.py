5032c79e9f06b998eb18b886f53e0f0cf97b8157
Mukhammad Karimov <hey@karimoff.me>
2021-12-26 15:28:35 +0500

    Day 6

--- medium/198_house_robber.py
+++ medium/198_house_robber.py
@@ -1,6 +1,8 @@
 # https://leetcode.com/problems/house-robber/
+from typing import List
 
-class Solution:
+
+class Solution1:
     def rob(self, nums: List[int]) -> int:
         # if there are no houses, return 0.
         if not nums:
@@ -11,12 +13,12 @@ class Solution:
         # if there are two houses, rob the one with more money return the value.
         if len(nums) == 2:
             return max(nums)
-        
+
         # if there are more than two houses, return the max of value:
         dp = [0] * len(nums)
         dp[0], dp[1] = nums[0], max(nums[0], nums[1])
         for i in range(2, len(nums)):
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
         return dp[-1]
 
 
@@ -24,11 +26,11 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) <= 2:
             return max(nums)
-        
+
         L = len(nums)
         a, b = nums[0], max(nums[0], nums[1])
-        
+
         for i in range(2, L):
             a, b = b, max(a + nums[i], b)
-        
+
         return b
